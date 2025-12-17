fa08b95a64ce0ffaeaca88ba47ca65e6a8d3643f
HubertUseCase <86836005+agegeekokou@users.noreply.github.com>
2025-12-17 09:48:50 +0100

    Update operator.c

---
+++ TP4/src/operator.c
@@ -0,0 +1,16 @@
+#include "operator.h"
+
+int somme(int num1, int num2) { return num1 + num2; }
+int difference(int num1, int num2) { return num1 - num2; }
+int produit(int num1, int num2) { return num1 * num2; }
+int quotient(int num1, int num2) { return num1 / num2; }
+int modulo(int num1, int num2) { return num1 % num2; }
+int et(int num1, int num2) { return num1 & num2; }
+int ou(int num1, int num2) { return num1 | num2; }
+int negation(int num1) { return ~num1; }
+
+
+
+
+
+
